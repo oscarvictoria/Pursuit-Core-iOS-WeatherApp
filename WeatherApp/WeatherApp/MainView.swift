d01e039205929f2eb28753c41a97ca146acd5767
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-30 18:45:41 -0500

    updated

--- WeatherApp/WeatherApp/MainView.swift
+++ WeatherApp/WeatherApp/MainView.swift
@@ -11,5 +11,6 @@ import UIKit
 class MainView: UIView {
 
   
+    
 
 }
--- WeatherApp/WeatherApp/Secrets/Secrets.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Secrets.swift
-//  WeatherApp
-//
-//  Created by Oscar Victoria Gonzalez  on 1/30/20.
-//  Copyright © 2020 David Rifkin. All rights reserved.
-//
-
-import Foundation
